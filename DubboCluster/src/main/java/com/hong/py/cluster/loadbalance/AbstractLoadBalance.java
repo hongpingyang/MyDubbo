d968c424adbb204c8979cdd868a5bc6ae2319108
hongpy <hpymn123@163.com>
2020-01-04 12:44:38 +0800

    loadbalance

--- DubboCluster/src/main/java/com/hong/py/cluster/loadbalance/AbstractLoadBalance.java
+++ DubboCluster/src/main/java/com/hong/py/cluster/loadbalance/AbstractLoadBalance.java
@@ -1,6 +1,7 @@
 package com.hong.py.cluster.loadbalance;
 
 import com.hong.py.cluster.LoadBalance;
+import com.hong.py.commonUtils.Constants;
 import com.hong.py.commonUtils.URL;
 import com.hong.py.rpc.Invocation;
 import com.hong.py.rpc.Invoker;
@@ -26,9 +27,26 @@ import java.util.List;
  **/
 public abstract class AbstractLoadBalance implements LoadBalance {
 
-
     @Override
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
-        return null;
+        if (invokers == null || invokers.isEmpty()) {
+            return null;
+        }
+        if (invokers.size() == 1) {
+            return invokers.get(0);
+        }
+        return doSelect(invokers, url, invocation);
+    }
+
+    protected abstract <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation);
+
+    /**
+     * 获取权重
+     * @param invoker
+     * @param invocation
+     * @return
+     */
+    protected int getWeight(Invoker<?> invoker, Invocation invocation) {
+        return invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
     }
 }
--- DubboCluster/src/main/java/com/hong/py/cluster/loadbalance/RandomLoadBalance.java
+++ DubboCluster/src/main/java/com/hong/py/cluster/loadbalance/RandomLoadBalance.java
@@ -1,5 +1,12 @@
 package com.hong.py.cluster.loadbalance;
 
+import com.hong.py.commonUtils.URL;
+import com.hong.py.rpc.Invocation;
+import com.hong.py.rpc.Invoker;
+
+import java.util.List;
+import java.util.Random;
+
 /**
  * 文件描述
  *
@@ -17,5 +24,41 @@ package com.hong.py.cluster.loadbalance;
  * Copyright © 2020 hongpy Technologies Inc. All Rights Reserved
  **/
 public class RandomLoadBalance extends AbstractLoadBalance {
+
+    private final Random random = new Random();
+
+    @Override
+    protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
+        int length = invokers.size();
+        int totalWeight=0;
+        boolean sameWeight=true;
+
+        for (int i = 0; i < length; i++) {
+            int weight = getWeight(invokers.get(i), invocation);
+            totalWeight+=weight;
+            if (sameWeight&&length > ++i) {
+                int weight2 = getWeight(invokers.get(i), invocation);
+                totalWeight+=weight2;
+                if (weight != weight2) {
+                    sameWeight=false;
+                }
+            }
+        }
+
+        if (totalWeight > 0 && !sameWeight) {
+
+            int offest=random.nextInt(totalWeight);
+
+            for (int i = 0; i < length; i++) {
+                offest-=getWeight(invokers.get(i),invocation);
+                if (offest<0){
+                    return invokers.get(i);
+                }
+            }
+        }
+
+        return invokers.get(random.nextInt(length));
+    }
+
 
 }
