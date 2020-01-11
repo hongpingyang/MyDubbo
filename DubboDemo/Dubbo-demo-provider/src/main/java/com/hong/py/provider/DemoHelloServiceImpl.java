92ec3ed771cbd2fecb6dd810d81df453e7e57978
Hongpy <hpymn123@163.com>
2020-01-11 09:57:45 +0800

    Demo

--- DubboDemo/Dubbo-demo-provider/src/main/java/com/hong/py/provider/DemoHelloServiceImpl.java
+++ DubboDemo/Dubbo-demo-provider/src/main/java/com/hong/py/provider/DemoHelloServiceImpl.java
@@ -7,6 +7,7 @@ import com.hong.py.demo.DemoHelloServiceforHpy;
 public class DemoHelloServiceImpl implements DemoHelloServiceforHpy {
     @Override
     public String sayHello(String name) {
+        System.out.println("this is returned msg ["+name+"]");
         return "this is returned msg ["+name+"]";
     }
 }
